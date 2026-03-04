e3845bc4924698c920dfa9acbe657778d8d7c3e4
vahagnvardanyan1 <vahagn.v.vardanyan@gmail.com>
2026-03-04 19:38:22 +0400

    Implement WOW Website Components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <p>© 2023 World of Web. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
